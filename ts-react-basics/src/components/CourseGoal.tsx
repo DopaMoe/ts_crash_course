5aec7f3cc52b2cb76ffe4b47f7e2cf946dc65288
DopaMoe <mohammad.o.felemban@gmail.com>
2024-10-03 11:55:03 +0300

    arrow function component

--- ts-react-basics/src/components/CourseGoal.tsx
+++ ts-react-basics/src/components/CourseGoal.tsx
@@ -1,5 +1,10 @@
 // good practice to add type keyword when dealing with children! so that the compiler knows that this is a react package
-import {type PropsWithChildren, type ReactNode} from "react";
+import {
+    // type FC, generic type used to pass props with arrow function
+    type ReactNode
+    // type PropsWithChildren,
+
+} from "react";
 
 interface CourseGoalProps {
     title: string;
@@ -9,6 +14,8 @@ interface CourseGoalProps {
 // alternative way to define properties with childrens
 // define a generic type with PropsWithChildren and put all the extra props inside
 // type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+
 export default function CourseGoal({title, children}: CourseGoalProps) {
     return (
         <article>
@@ -20,3 +27,19 @@ export default function CourseGoal({title, children}: CourseGoalProps) {
         </article>
     );
 }
+
+// alternative way to define a component with an arrow funciton
+// const CourseGoal: FC<CourseGoalProps> = ({title, children}) => {
+//     return (
+//         <article>
+//             <div>
+//                 <h2>{title}</h2>
+//                 {children}
+//             </div>
+//             <button>Delete</button>
+//         </article>
+//     );
+// }
+//
+// export default CourseGoal;
+
