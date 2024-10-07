3f9645ae85d4d3e5d1b88aa8d17e5474571cef87
DopaMoe <mohammad.o.felemban@gmail.com>
2024-10-07 14:51:59 +0300

    infobox

--- ts-react-basics/src/components/CourseGoalList.tsx
+++ ts-react-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal.tsx";
+import InfoBox from "./InfoBox.tsx";
 import {type Goals} from "../App.tsx";
+import {type ReactNode} from "react";
 
 interface CourseGoalListProps {
     goals: Goals[];
@@ -7,18 +9,32 @@ interface CourseGoalListProps {
 };
 
 export default function CourseGoalList({goals, onDeleteGoal}: CourseGoalListProps) {
+    if (goals.length === 0) {
+        return (
+            <InfoBox mode={"hint"}>
+                Start Adding Some Goals!
+            </InfoBox>
+        );
+    }
+    let warningBox: ReactNode;
+    if (goals.length >= 4) {
+        warningBox = <InfoBox mode={"warning"} severity={"low"}>You're doing too much man!</InfoBox>;
+    }
     return (
-        <ul>
-            {
-                goals.map(goal => (
-                        <li key={goal.id}>
-                            <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
-                                <p>{goal.description}</p>
-                            </CourseGoal>
-                        </li>
+        <>
+            {warningBox}
+            <ul>
+                {
+                    goals.map(goal => (
+                            <li key={goal.id}>
+                                <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
+                                    <p>{goal.description}</p>
+                                </CourseGoal>
+                            </li>
+                        )
                     )
-                )
-            }
-        </ul>
+                }
+            </ul>
+        </>
     );
 }
